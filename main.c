55608d4f4924d0aae4372c910456d832aed17fbe
Bhuvanchandra DV <bhuvanchandra.dv@rapyuta-robotics.com>
2019-07-18 17:56:56 +0900

    librevpi_test: initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "revpi.h"
+
+int main(void) {
+    revpi_peripheral o_1, i_1;
+    if ((revpi_init(&o_1) < 0) || (revpi_init(&i_1) < 0)) {
+        printf("failed to init\n");
+    } else {
+        revpi_set_do_level(&o_1, 1);
+        sleep(1);
+        printf("I_1 pin state: %d\n", revpi_get_di_level(&i_1));
+    }
+
+    return 0;
+}
