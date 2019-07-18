f095a8224c7a10ca67efe537f6a770711cf43967
Bhuvanchandra DV <bhuvanchandra.dv@rapyuta-robotics.com>
2019-07-18 18:03:09 +0900

    clean formatting

--- main.c
+++ main.c
@@ -5,14 +5,14 @@
 #include "revpi.h"
 
 int main(void) {
-    revpi_peripheral o_1, i_1;
-    if ((revpi_init(&o_1) < 0) || (revpi_init(&i_1) < 0)) {
-        printf("failed to init\n");
-    } else {
-        revpi_set_do_level(&o_1, 1);
-        sleep(1);
-        printf("I_1 pin state: %d\n", revpi_get_di_level(&i_1));
-    }
+  revpi_peripheral o_1, i_1;
+  if ((revpi_init(&o_1) < 0) || (revpi_init(&i_1) < 0)) {
+    printf("failed to init\n");
+  } else {
+    revpi_set_do_level(&o_1, 1);
+    sleep(1);
+    printf("I_1 pin state: %d\n", revpi_get_di_level(&i_1));
+  }
 
-    return 0;
+  return 0;
 }
